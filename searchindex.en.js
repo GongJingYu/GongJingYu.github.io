25397c751a002aa2864f4dff6c78afa640be622f
GongJingYu <2388873192@qq.com>
2026-04-05 14:14:20 +0800

    Initial commit

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Project",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
