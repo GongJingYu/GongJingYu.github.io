4808128d16005ef602aaa7225f90e280ed185ef1
GongJingYu <2388873192@qq.com>
2026-04-05 14:31:02 +0800

    Update content

--- searchindex.en.js
+++ searchindex.en.js
@@ -1,11 +1,51 @@
 var relearn_searchindex = [
   {
+    "breadcrumb": "My New Hugo Project \u003e Shell Scripts",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Basic",
+    "uri": "/shell-scripts/basic/index.html"
+  },
+  {
     "breadcrumb": "",
     "content": "",
     "description": "",
     "tags": [],
     "title": "My New Hugo Project",
     "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Shell Scripts",
+    "uri": "/shell-scripts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project \u003e Commands",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Grep",
+    "uri": "/commands/grep/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project \u003e Commands",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Ls",
+    "uri": "/commands/ls/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Project",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Commands",
+    "uri": "/commands/index.html"
   },
   {
     "breadcrumb": "My New Hugo Project",
